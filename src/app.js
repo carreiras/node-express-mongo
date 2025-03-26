fde097baa0b492c001571dd1cda8d0f25c8a2593
Ewerton Carreira <ewertoncarreira@gmail.com>
2025-03-26 14:26:25 -0300

    Buscando e atualizando registros

--- src/app.js
+++ src/app.js
@@ -8,6 +8,12 @@ const livros = [
   { id: 3, titulo: "Neuromancer" },
 ];
 
+function buscaLivro(id) {
+  return livros.findIndex((livro) => {
+    return livro.id === Number(id);
+  });
+}
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -18,9 +24,21 @@ app.get("/livros", (req, res) => {
   res.status(200).json(livros);
 });
 
+app.get("/livros/:id", (req, res) => {
+  const livro = buscaLivro(req.params.id);
+  res.status(200).json(livros[livro]);
+});
+
 app.post("/livros", (req, res) => {
   livros.push(req.body);
   res.status(201).send("Livro cadastrado com sucesso!");
+});
+
+app.put("/livros/:id", (req, res) => {
+  const livro = buscaLivro(req.params.id);
+
+  livros[livro].titulo = req.body.titulo;
+  res.status(200).json(livros[livro]);
 });
 
 export default app;
