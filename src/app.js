4c4774d18d512552737fc5cdbe4ea51aea68fd39
Ewerton Carreira <ewertoncarreira@gmail.com>
2025-03-26 12:51:59 -0300

    Criando a rota livros

--- src/app.js
+++ src/app.js
@@ -2,8 +2,18 @@ import express from "express";
 
 const app = express();
 
+const livros = [
+    { id: 1, titulo: "O Senhor dos Anéis" },
+    { id: 2, titulo: "Fundação" },
+    { id: 3, titulo: "Neuromancer" },
+]
+
 app.get("/", (req, res) => {
     res.status(200).send("Hello World\n");
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
 });
 
 export default app;
