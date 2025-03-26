f1dd635508c885ee4bf438f7276beccf390082ee
Ewerton Carreira <ewertoncarreira@gmail.com>
2025-03-26 13:21:48 -0300

    Criando registros com POST

--- src/app.js
+++ src/app.js
@@ -3,17 +3,24 @@ import express from "express";
 const app = express();
 
 const livros = [
-    { id: 1, titulo: "O Senhor dos Anéis" },
-    { id: 2, titulo: "Fundação" },
-    { id: 3, titulo: "Neuromancer" },
-]
+  { id: 1, titulo: "O Senhor dos Anéis" },
+  { id: 2, titulo: "Fundação" },
+  { id: 3, titulo: "Neuromancer" },
+];
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.status(200).send("Hello World\n");
+  res.status(200).send("Hello World\n");
 });
 
 app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
+  res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("Livro cadastrado com sucesso!");
 });
 
 export default app;
