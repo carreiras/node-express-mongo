83f9eca8af7d532f1409c79b008476af866553aa
Ewerton Carreira <ewertoncarreira@gmail.com>
2025-04-06 12:02:22 -0300

    Unindo livros e autores

--- src/controllers/autorController.js
+++ src/controllers/autorController.js
@@ -1,71 +1,71 @@
-import autor from "../models/Autor.js";
+import { autor } from "../models/Autor.js";
 
 class AutorController {
-    static async listarAutores(req, res) {
-        try {
-            const listaAutores = await autor.find();
-            res.status(200).json(listaAutores);
-        } catch (error) {
-            res.status(500).json({
-                message: "Erro ao listar autores",
-                error: error.message,
-            });
-        }
+  static async listarAutores(req, res) {
+    try {
+      const listaAutores = await autor.find();
+      res.status(200).json(listaAutores);
+    } catch (error) {
+      res.status(500).json({
+        message: "Erro ao listar autores",
+        error: error.message,
+      });
     }
+  }
 
-    static async listarAutorPorId(req, res) {
-        try {
-            const id = req.params.id;
-            const autorEncontrado = await autor.findById(id);
-            res.status(200).json(autorEncontrado);
-        } catch (error) {
-            res.status(500).json({
-                message: "Erro ao listar autor",
-                error: error.message,
-            });
-        }
+  static async listarAutorPorId(req, res) {
+    try {
+      const id = req.params.id;
+      const autorEncontrado = await autor.findById(id);
+      res.status(200).json(autorEncontrado);
+    } catch (error) {
+      res.status(500).json({
+        message: "Erro ao listar autor",
+        error: error.message,
+      });
     }
+  }
 
-    static async cadastrarAutor(req, res) {
-        try {
-            const novoAutor = await autor.create(req.body);
-            res.status(201).json({
-                message: "Autor cadastrado com sucesso!",
-                autor: novoAutor,
-            });
-        } catch (error) {
-            res.status(500).json({
-                message: "Erro ao cadastrar autor",
-                error: error.message,
-            });
-        }
+  static async cadastrarAutor(req, res) {
+    try {
+      const novoAutor = await autor.create(req.body);
+      res.status(201).json({
+        message: "Autor cadastrado com sucesso!",
+        autor: novoAutor,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: "Erro ao cadastrar autor",
+        error: error.message,
+      });
     }
+  }
 
-    static async atualizarAutor(req, res) {
-        try {
-            const id = req.params.id;
-            await autor.findByIdAndUpdate(id, req.body);
-            res.status(204).json({message: "Autor atualizado com sucesso!"});
-        } catch (error) {
-            res.status(500).json({
-                message: "Erro ao atualizar autor",
-                error: error.message,
-            });
-        }
+  static async atualizarAutor(req, res) {
+    try {
+      const id = req.params.id;
+      await autor.findByIdAndUpdate(id, req.body);
+      res.status(204).json({ message: "Autor atualizado com sucesso!" });
+    } catch (error) {
+      res.status(500).json({
+        message: "Erro ao atualizar autor",
+        error: error.message,
+      });
     }
+  }
 
-    static async excluirAutor(req, res) {
-        try {
-            const id = req.params.id;
-            await autor.findByIdAndDelete(id);
-            res.status(204).json({message: "Autor excluído com sucesso!"});
-        } catch (error) {
-            res.status(500).json({
-                message: "Erro ao excluir autor",
-                error: error.message,
-            });
-        }
+  static async excluirAutor(req, res) {
+    try {
+      const id = req.params.id;
+      await autor.findByIdAndDelete(id);
+      res.status(204).json({ message: "Autor excluído com sucesso!" });
+    } catch (error) {
+      res.status(500).json({
+        message: "Erro ao excluir autor",
+        error: error.message,
+      });
     }
+  }
 }
 
 export default AutorController;
--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,71 +1,80 @@
+import { autor } from "../models/Autor.js";
 import livro from "../models/Livro.js";
 
 class LivroController {
-    static async listarLivros(req, res) {
-        try {
-            const listaLivros = await livro.find();
-            res.status(200).json(listaLivros);
-        } catch (error) {
-            res.status(500).json({
-                message: "Erro ao listar livros",
-                error: error.message,
-            });
-        }
+  static async listarLivros(req, res) {
+    try {
+      const listaLivros = await livro.find();
+      res.status(200).json(listaLivros);
+    } catch (error) {
+      res.status(500).json({
+        message: "Erro ao listar livros",
+        error: error.message,
+      });
     }
+  }
 
-    static async listarLivroPorId(req, res) {
-        try {
-            const id = req.params.id;
-            const livroEncontrado = await livro.findById(id);
-            res.status(200).json(livroEncontrado);
-        } catch (error) {
-            res.status(500).json({
-                message: "Erro ao listar livro",
-                error: error.message,
-            });
-        }
+  static async listarLivroPorId(req, res) {
+    try {
+      const id = req.params.id;
+      const livroEncontrado = await livro.findById(id);
+      res.status(200).json(livroEncontrado);
+    } catch (error) {
+      res.status(500).json({
+        message: "Erro ao listar livro",
+        error: error.message,
+      });
     }
+  }
 
-    static async cadastrarLivro(req, res) {
-        try {
-            const novoLivro = await livro.create(req.body);
-            res.status(201).json({
-                message: "Livro cadastrado com sucesso!",
-                livro: novoLivro,
-            });
-        } catch (error) {
-            res.status(500).json({
-                message: "Erro ao cadastrar livro",
-                error: error.message,
-            });
-        }
-    }
+  static async cadastrarLivro(req, res) {
+    const novoLivro = req.body;
 
-    static async atualizarLivro(req, res) {
-        try {
-            const id = req.params.id;
-            await livro.findByIdAndUpdate(id, req.body);
-            res.status(204).json({message: "Livro atualizado com sucesso!"});
-        } catch (error) {
-            res.status(500).json({
-                message: "Erro ao atualizar livro",
-                error: error.message,
-            });
-        }
-    }
+    try {
+      const autorEncontrado = await autor.findById(novoLivro.autor);
+      const livroCompleto = {
+        ...novoLivro,
+        autor: { ...autorEncontrado._doc },
+      };
+      const livroCriado = await livro.create(livroCompleto);
 
-    static async excluirLivro(req, res) {
-        try {
-            const id = req.params.id;
-            await livro.findByIdAndDelete(id);
-            res.status(204).json({message: "Livro excluído com sucesso!"});
-        } catch (error) {
-            res.status(500).json({
-                message: "Erro ao excluir livro",
-                error: error.message,
-            });
-        }
+      res.status(201).json({
+        message: "Livro cadastrado com sucesso!",
+        livro: livroCriado,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: "Erro ao cadastrar livro",
+        error: error.message,
+      });
     }
+  }
+
+  static async atualizarLivro(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndUpdate(id, req.body);
+      res.status(204).json({ message: "Livro atualizado com sucesso!" });
+    } catch (error) {
+      res.status(500).json({
+        message: "Erro ao atualizar livro",
+        error: error.message,
+      });
+    }
+  }
+
+  static async excluirLivro(req, res) {
+    try {
+      const id = req.params.id;
+      await livro.findByIdAndDelete(id);
+      res.status(204).json({ message: "Livro excluído com sucesso!" });
+    } catch (error) {
+      res.status(500).json({
+        message: "Erro ao excluir livro",
+        error: error.message,
+      });
+    }
+  }
 }
 
 export default LivroController;
